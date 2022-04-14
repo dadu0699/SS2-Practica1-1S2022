355c93ef8c38df70f2959e75e5813b8c071f9f5a
Didier Domínguez <dadu0699@hotmail.com>
2022-04-13 19:15:55 -0600

    [ADD] Report 4

---
+++ report4.R
@@ -0,0 +1,20 @@
+# install.packages("tidyverse")
+library("tidyverse")
+
+# Se lee el archivo de datos y se guarda en una variable
+datos_presion_sanguinea <- read.csv("./Data/PresionSanguineaEdad.csv")
+
+# Función que devuelve las primeras n filas del conjunto de datos.
+head(datos_presion_sanguinea)
+
+pairs(datos_presion_sanguinea)
+cor(datos_presion_sanguinea)
+
+# y = Estimate Std(Intercept) + Estimate Std(Edad) x
+# y = 98.7147 + 0.9709x
+regresion <- lm(Systolic.Blood.Pressure ~ Edad, data = datos_presion_sanguinea)
+summary(regresion)
+
+plot(datos_presion_sanguinea$Edad, datos_presion_sanguinea$Systolic.Blood.Pressure, 
+    xlab='Edad', ylab='Presión sanguínea')
+abline(regresion)
